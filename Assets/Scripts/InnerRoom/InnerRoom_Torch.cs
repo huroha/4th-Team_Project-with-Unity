692294c5fe4eb2f48e00077320bd3f061cd992f1
Minseok Kwon <kmsms4050@gmail.com>
2023-06-22 12:07:05 +0900

    innerRoom 수정중
    중간저장용

--- Assets/Scripts/InnerRoom/InnerRoom_Torch.cs
+++ Assets/Scripts/InnerRoom/InnerRoom_Torch.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class InnerRoom_Torch : MonoBehaviour
 {
     // Start is called before the first frame
-    [SerializeField] private GameObject objectToActivate; // 활성화할 오브젝트
+    public GameObject objectToActivate; // 활성화할 오브젝트
+    public GameObject objectToActivate2;
     [SerializeField] private string triggerTag = "Player"; // Trigger를 발생시키는 오브젝트의 태그
+    public GameObject objectToDeAtivate;    // 비활성화 오브젝트
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -14,6 +17,9 @@ public class InnerRoom_Torch : MonoBehaviour
         {
             Debug.Log("torch on");
             objectToActivate.SetActive(true);
+            objectToActivate2.SetActive(true);
+            objectToDeAtivate.SetActive(false);
+
         }
     }
 }
